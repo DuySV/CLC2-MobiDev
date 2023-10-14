9fd3dd9c7502eba7bd17f19ff7be41e4aafd92cf
Dương Nhất Duy <duy.dn.63cntt@ntu.edu.vn>
2023-10-14 16:21:32 +0700

    .
    .

--- SimpleCalc/app/src/main/java/com/example/simplecalc/MainActivity.java
+++ SimpleCalc/app/src/main/java/com/example/simplecalc/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.simplecalc;
 
 import static com.example.simplecalc.R.id.btnPlus;
 
+import com.example.simplecalc.R;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,56 +12,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText edtX, edtY;
     TextView tvZ;
     Button btnPlus, btnMinus, btnMultiply, btnDivide;
+    int num1,num2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findView();
+        btnPlus.setOnClickListener(this);
+        btnMinus.setOnClickListener(this);
+        btnMultiply.setOnClickListener(this);
+        btnDivide.setOnClickListener(this);
 
-        int num1, num2;
-        num1 = getInt(edtX);
-        num2 = getInt(edtY);
-        btnPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                tvZ.setText("Answer x + y =" + (num1 + num2));
-            }
-        });
-        btnMinus.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                tvZ.setText("Answer x - y =" + (num1 - num2));
-            }
-        });
-        btnMultiply.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                tvZ.setText("Answer x * y =" + (num1 * num2));
-            }
-        });
-        btnDivide.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                if (num2 == 0) {
-                    tvZ.setText("Answer=ERROR");
-                }
-                tvZ.setText("Answer x / y =" + (num1 / num2));
-            }
-        });
     }
-
-    public int getInt(EditText editText) {
-        if (editText.getText().toString().equals("")) {
-            Toast.makeText(this, "Enter number", Toast.LENGTH_SHORT).show();
-            return 0;
-        } else
-            return Integer.parseInt(editText.getText().toString());
-    }
-
     public void findView() {
         edtX = findViewById(R.id.edtX);
         edtY = findViewById(R.id.edtY);
@@ -70,4 +40,35 @@ public class MainActivity extends AppCompatActivity {
         btnMultiply = findViewById(R.id.btnMultiply);
         btnDivide = findViewById(R.id.btnDivide);
     }
+    @Override
+    public void onClick(View v) {
+        num1 = getInt(edtX);
+        num2 = getInt(edtY);
+        if (v.getId() == R.id.btnPlus){
+            tvZ.setText("Answer x + y =" + (num1 + num2));
+        }
+        else if (v.getId() == R.id.btnMinus){
+            tvZ.setText("Answer x - y =" + (num1 - num2));
+        }
+        else if (v.getId() == R.id.btnMultiply){
+            tvZ.setText("Answer x * y =" + (num1 * num2));
+        }
+        else if (v.getId() == R.id.btnDivide){
+            if (num2 == 0) {
+                   tvZ.setText("Answer=ERROR");
+              }
+               tvZ.setText("Answer x / y =" + (float)num1 / (float)num2);
+        }
+    }
+    public int getInt(EditText editText) {
+        if (editText.getText().toString().equals("")) {
+            Toast.makeText(this, "Enter number", Toast.LENGTH_SHORT).show();
+            return 0;
+        } else
+            return Integer.parseInt(editText.getText().toString());
+    }
+
+
+
+
 }
